e941c267d5987036a9f17a848952ce6dc5ce2154
YuvadharshiniM <yuvadharshinim05@gmail.com>
2025-09-16 12:18:48 +0530

    [169] [Time Beats: 73.83%] [Memory Beats: 50.69%] - LeetPush

---
+++ Majority-Element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int voter = nums[0];
+        int vc = 1;
+        for(int i=1;i<nums.length;i++){
+            if(vc==0) voter = nums[i];
+            if(voter==nums[i]) vc = vc+1;
+            else vc = vc -1;
+        }
+        return voter;
+    }
+}
